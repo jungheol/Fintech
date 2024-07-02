a6f902bdab6edc0d6aaacdd88a9d671bd8b5bd0f
jungheol <jungheol@naver.com>
2024-07-02 20:52:16 +0900

    domain에서 필요한 framework 추가 및 mysql 세팅

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,5 +1,13 @@
-plugins {}
+plugins {
+    kotlin("plugin.jpa")
+}
 
 version = "0.0.1"
 
-repositories {}
+repositories {
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
+        implementation("mysql:mysql-connector-java")
+        implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    }
+}
